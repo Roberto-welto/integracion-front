87a17092bf27de08c8718583d5fc1190233bea1b
Roberto-welto <rgonzalez@arkho.tech>
2020-07-14 21:33:53 -0400

    added changes to front images

--- sistema-adopcion/src/app/app.module.ts
+++ sistema-adopcion/src/app/app.module.ts
@@ -21,6 +21,8 @@ import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { DonacionModalComponent } from './main/donacion/donacion-modal/donacion-modal.component';
 import { NgxPayPalModule } from 'ngx-paypal';
+import { ModalMascotaComponent } from './main/mascotas/index/modal-mascota/modal-mascota.component';
+import { ModalImgComponent } from './main/mascotas/index/modal-img/modal-img.component';
 
 @NgModule({
   declarations: [
@@ -36,10 +38,14 @@ import { NgxPayPalModule } from 'ngx-paypal';
     ProgressBarComponent,
     SafeHtmlPipe,
     DonacionModalComponent,
+    ModalMascotaComponent,
+    ModalImgComponent,
   ],
   entryComponents: [
     DialogComponent,
-    DonacionModalComponent
+    DonacionModalComponent,
+    ModalMascotaComponent,
+    ModalImgComponent
   ],
   imports: [
     BrowserModule,
